66fc6c842ee7064baea32a05cb5db3d9a4a421b9
Juzy-Lagrange <ntdpy.dame.py@gmail.com>
2022-12-15 02:37:18 +0300

    write tests

--- main.cpp
+++ main.cpp
@@ -1,29 +1,6 @@
-#include <iostream>|
-#include <cassert>
-
-#include "lib/Calculator.h"
-
-void Test_Calc() {
-
-    std::string TestN;
-    Calculator* Calc;
-
-    Calc = new Calculator;
-    assert(Calc->calculate("3 2 +") == 5 );
-    delete Calc;
-
-    Calc= new Calculator;
-    assert(Calc->calculate("1 0 + 1 -") == 0);
-    delete Calc;
-
-    Calc= new Calculator;
-    assert(Calc->calculate("8 9 + 1 7 - * ") == -102);
-    delete Calc;
-
-    printf("All tests Passed: OK");
-}
+#include <iostream>
 
 int main() {
-    Test_Calc();
+    std::cout << "Hello World!";
     return 0;
 }
